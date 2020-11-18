7ec6d3b7235a25e1a051f8b70066339d98dc1356
Benjamín Colin <benjaother@gmail.com>
2020-11-17 22:09:07 -0600

    - valores iniciales

--- src/main/java/com/upemor/petsorerest/component/AdminCreator.java
+++ src/main/java/com/upemor/petsorerest/component/AdminCreator.java
@@ -5,7 +5,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.upemor.petsorerest.model.Category;
+import com.upemor.petsorerest.model.Tag;
 import com.upemor.petsorerest.model.User;
+import com.upemor.petsorerest.service.CategoryService;
+import com.upemor.petsorerest.service.TagService;
 import com.upemor.petsorerest.service.UserService;
 
 @Component
@@ -14,12 +18,36 @@ public class AdminCreator {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private TagService tagService;
+	
+	@Autowired
+	private CategoryService catService;
+	
 	@PostConstruct
 	public void adminCreate() {
 		if(!userService.existsUserByEmail("admin@localhost.com")) {
 			User user = new User("admin","admin","admin","admin@localhost.com","admin",true,"ADMIN", null);
 			userService.createUser(user);
 		}
+		
+		Category category = new Category();
+		category.setName("De compañia");
+		
+		Tag tag = new Tag();
+		tag.setName("Raza pequeña");
+		
+		Category category2 = new Category();
+		category2.setName("De Rescate");
+		
+		Tag tag2 = new Tag();
+		tag2.setName("Raza Grande");
+		
+		tagService.createTag(tag);
+		catService.createCategory(category);
+		tagService.createTag(tag2);
+		catService.createCategory(category2);
+		
 	}
 
 }
